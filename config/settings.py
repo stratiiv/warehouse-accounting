8c4fe92af4cda588d177ee76b759e737ed370423
stratiiv <stratiyvalter@gmail.com>
2023-06-27 20:49:28 +0300

    add models

--- config/settings.py
+++ config/settings.py
@@ -130,3 +130,16 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Base url to serve media files
+MEDIA_URL = '/media/'
+
+# Path where media is stored
+MEDIA_ROOT = BASE_DIR / 'media/'
+
+# importing local settings if present
+try:
+    from .local_settings import *
+except ImportError:
+    pass
--- products/models.py
+++ products/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.id} {self.name}"
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='products')
+    preview = models.ImageField(upload_to='products/previews')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.id} {self.name}"
+
