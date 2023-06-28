f4d9f4d100a3b308e92b50a791f6c9834774fe64
stratiiv <stratiyvalter@gmail.com>
2023-06-28 21:21:59 +0300

    add description field to category

--- products/models.py
+++ products/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return f"{self.id} {self.name}"
