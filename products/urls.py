0ce1e2f4f6a967b63dd44f2ef06aadf3edc4da8a
stratiiv <stratiyvalter@gmail.com>
2023-07-17 10:15:33 +0300

    move api views into separate module

--- products/urls.py
+++ products/urls.py
@@ -1,11 +1,14 @@
 from rest_framework.routers import DefaultRouter
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import ProductViewSet, CategoryViewSet
+from django.urls import path, include
+from . import api_views
 
 
 router = DefaultRouter()
-router.register(r'products', ProductViewSet)
-router.register(r'categories', CategoryViewSet)
-urlpatterns = router.urls
+router.register(r'products', api_views.ProductViewSet)
+router.register(r'categories', api_views.CategoryViewSet)
+urlpatterns = [
+    path('api/v1/', include(router.urls)),
+]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
