aafeafed620169d287f1371b2293b74657edfa79
stratiiv <stratiyvalter@gmail.com>
2023-07-19 13:31:33 +0300

    add models docstrings

--- products/models.py
+++ products/models.py
@@ -4,6 +4,9 @@ from .services import generate_preview_image, validate_image
 
 
 class Category(models.Model):
+    """
+    Model representing a category for products.
+    """
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
 
@@ -12,9 +15,13 @@ class Category(models.Model):
 
 
 class Product(models.Model):
+    """
+    Model representing a product in the inventory.
+    """
     name = models.CharField(max_length=255)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='products', validators=[validate_image])
+    image = models.ImageField(upload_to='products',
+                              validators=[validate_image])
     preview = models.ImageField(upload_to='products/previews', blank=True,
                                 null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -28,4 +35,3 @@ class Product(models.Model):
 
     def __str__(self):
         return f"{self.id} {self.name}"
-
